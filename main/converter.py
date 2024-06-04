1f9441419a5fa0f86b4bde0fda4da09fa4ff30d9
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-06-04 18:02:45 +0530

    Update converter.py

--- main/converter.py
+++ main/converter.py
@@ -1,32 +1,28 @@
 import os
 import time
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
-from config import DOWNLOAD_LOCATION, ADMIN
+from config import DOWNLOAD_LOCATION, ADMIN, CAPTION
 from main.utils import progress_message, humanbytes
 from moviepy.editor import VideoFileClip
 
-# Initialize Pyrogram client
 app = Client("video_to_mp3_bot")
 
 # /convert command handler
-@Client.on_message(filters.private & filters.command("convert") & filters.user(ADMIN))
+@app.on_message(filters.private & filters.command("convert") & filters.user(ADMIN))
 async def convert_to_mp3(bot, msg):
-    chat_id = msg.chat.id
     await msg.reply_text("🔄 Please send the video file you want to convert to MP3.")
 
 # Handler for receiving the video file
 @app.on_message(filters.private & filters.video & filters.user(ADMIN))
 async def receive_video(bot, msg: Message):
-    chat_id = msg.chat.id
     video = msg.video
     if video:
+        sts = await msg.reply_text("📥 Video received. Converting to MP3...")
         try:
-            await msg.reply_text("📥 Video received. Converting to MP3...")
-            sts = await msg.reply_text("🔄 Converting...")
             c_time = time.time()
             downloaded = await bot.download_media(
                 video,
+                file_name=os.path.join(DOWNLOAD_LOCATION, video.file_name),
                 progress=progress_message,
                 progress_args=("📥 Downloading...", sts, c_time)
             )
@@ -47,9 +43,9 @@ async def receive_video(bot, msg: Message):
             c_time = time.time()
             try:
                 await bot.send_audio(
-                    chat_id,
+                    msg.chat.id,
                     audio=mp3_path,
-                    caption=f"🎵 Converted MP3nn💽 Size: {filesize}",
+                    caption=f"🎵 Converted MP3\n\n💽 Size: {filesize}",
                     progress=progress_message,
                     progress_args=("🚀 Uploading MP3 file...", sts, c_time)
                 )
@@ -57,9 +53,14 @@ async def receive_video(bot, msg: Message):
                 await sts.edit(f"❌ Error during upload: {e}")
                 return
 
-            os.remove(downloaded)
-            os.remove(mp3_path)
+            try:
+                os.remove(downloaded)
+                os.remove(mp3_path)
+            except Exception as e:
+                await sts.edit(f"❌ Error during cleanup: {e}")
+                return
+
             await sts.delete()
             await msg.reply_text("✅ MP3 file converted and uploaded successfully!")
         except Exception as e:
-            print(f"❌ Error: {e}")
+            await sts.edit(f"❌ Error: {e}")
