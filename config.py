8b848060f6561b91cf05290147dd153c86d30d50
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-08-04 15:15:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ from os import environ
 API_ID = int(environ.get("API_ID", "14631157"))
 API_HASH = environ.get("API_HASH", "aa7c2b3be68a7488abdb9de6ce78d311")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7341561892:AAHDXnPLYUxW0Qh5XG8ynGGY1lKW8PumNqk")
+TG_MAX_FILE_SIZE = 2097152000  # 2GB for Telegram
+CHUNK_SIZE = 1024 * 1024  # 1MB
+PROCESS_MAX_TIMEOUT = 300  # 5 minutes
+CAPTION = "{file_name}\n\n💽 size: {file_size}\n🕒 duration: {duration} seconds"
 ADMIN = int(environ.get("ADMIN", "5380833276"))          
 CAPTION = environ.get("CAPTION", "video")
 
