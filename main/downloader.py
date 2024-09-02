caec63e99b970033fa63d8237260e2cfd0a85615
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-09-02 22:00:48 +0530

    Update downloader.py

--- main/downloader.py
+++ main/downloader.py
@@ -118,7 +118,7 @@ async def yt_callback_handler(bot, query):
     url = query.data.split('_', 3)[3]
 
     # Send initial download started message
-    download_message = await query.message.edit_text("⬇️ **Download started...**\n\n🎬 **{info_dict['title']}**")
+    download_message = await query.message.edit_text("⬇️ **Download started...**")
 
     def progress_hook(d):
         if d['status'] == 'downloading':
