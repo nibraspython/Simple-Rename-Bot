ecd3528045d9152f1da9cef03676562eb4f1d985
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-09-14 18:38:03 +0530

    Update downloader.py

--- main/downloader.py
+++ main/downloader.py
@@ -130,9 +130,8 @@ async def yt_callback_handler(bot, query):
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
             'preferedformat': 'mp4'
-        }],
-        'progress_hooks': [lambda d: download_progress_hook(d, download_message, query.message)]
-      }
+        }]
+    }
     
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
