7c660be5ae87073097bae8ccf440ce01f533919e
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-09-18 20:24:08 +0530

    Add files via upload

---
+++ ytdl_text.py
@@ -0,0 +1,16 @@
+YTDL_WELCOME_TEXT = """
+            **Youtube Video Downloader**
+
+    📹 ━━━━━━━━❰ **Feautures** ❱━━━━━━━━━━➣
+    
+    ➭📹 **Download Youtube Video In All Available Resolutions**
+    ➭🎧 **Download Audio In Highest Bitrate**
+    ➭⚡ **Fastest Youtube Downloading Library** 
+    ➭📤 **Fastest Uploading Library With Progress Tracking**
+
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ➯
+
+🔗 **Please Send Your Youtube Links To Start Downloading**
+
+🖼 [YouTube Downloader Logo](TELEGRAPH_IMAGE_URL)
+"""
