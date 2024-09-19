f4194baafae426caa49cc8453a7b4a4200c3d4ca
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-09-19 14:45:54 +0530

    Update ytdl_text.py

--- ytdl_text.py
+++ ytdl_text.py
@@ -3,11 +3,25 @@ YTDL_WELCOME_TEXT = """
 
     📹 ━━━━━━❰ **Feautures** ❱━━━━━━➣
     
-   ➭📹 **Download Youtube Video In All Available Resolutions\n**
-   ➭🎧 **Download Audio In Highest Bitrate\n**
-   ➭⚡ **Fastest Youtube Downloading Library\n** 
-   ➭📤 **Fastest Uploading Library With Progress Tracking\n**
-   ➭🖼 **Video Thumbnail And Description Support 📄**\n
+ ➭📹 **Download Youtube Video In All Available Resolutions\n**
+ ➭🎧 **Download Audio In Highest Bitrate\n**
+ ➭⚡ **Fastest Youtube Downloading Library\n** 
+ ➭📤 **Fastest Uploading Library With Progress Tracking\n**
+ ➭🖼 **Video Thumbnail And Description Support 📄**\n
    
 🔗 **Please Send Your Youtube Links To Start Downloading**
 """
+
+
+VID_TRIMMER_TEXT = """
+         **Video Trimmer **
+
+    ✂ ━━━━━━❰ **Feautures** ❱━━━━━━➣
+
+ ➭✂ **Video And Document Files Support\n**
+ ➭⚡ **Fast And Best Library\n**
+ ➭📥 **Download And Upload Progress Tracking** 📤\n
+
+❤ **Thanks To K-MAC For His Great Trimming Code.\n**
+
+**📂 Please Send Your Video Or Document To Trim.**
